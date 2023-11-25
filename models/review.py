04e89f9fd2fad52be3897e758432d2882fed2547
Yalelet Dessalegn <125442134+Yab8702@users.noreply.github.com>
2023-11-25 04:38:35 +0000

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+''' Review module for the HBNB project '''
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Text, ForeignKey
+from os import getenv
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    ''' The Review class '''
+    __tablename__ = 'reviews'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        text = Column(Text(1024), nullable=False)
+        place_id = Column(String(60),
+                          ForeignKey('places.id', ondelete='CASCADE'),
+                          nullable=False)
+        user_id = Column(String(60),
+                         ForeignKey('users.id', ondelete='CASCADE'),
+                         nullable=False)
+    else:
+        text = ''
+        place_id = ''
+        user_id = ''
