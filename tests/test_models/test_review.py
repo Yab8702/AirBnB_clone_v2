e300dd9d43c514b17f99f5d4a6f01c9017ca8a10
Yalelet Dessalegn <125442134+Yab8702@users.noreply.github.com>
2023-11-25 04:30:31 +0000

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -7,23 +7,18 @@ from models.review import Review
 class test_review(test_basemodel):
     """ """
 
-    def __init__(self, *args, **kwargs):
+    def setUp(self):
         """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+        self.new = Review(place_id="123-756", user_id="790-543", text="Lorem")
 
     def test_place_id(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        self.assertEqual(type(self.new.place_id), str)
 
     def test_user_id(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        self.assertEqual(type(self.new.user_id), str)
 
     def test_text(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+        self.assertEqual(type(self.new.text), str)
