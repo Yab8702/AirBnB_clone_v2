54dcc00e64714d6e0284cac6c012d17ce5e58c59
Yalelet Dessalegn <125442134+Yab8702@users.noreply.github.com>
2023-11-25 04:35:16 +0000

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+''' City Module for HBNB project '''
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    ''' The City Class '''
+    __tablename__ = 'cities'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60),
+                          ForeignKey('states.id', ondelete='CASCADE'),
+                          nullable=False)
+        places = relationship("Place", backref="cities", cascade="all, delete")
+    else:
+        name = ''
+        state_id = ''
